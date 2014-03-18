ca98a8a5e52c855fedad29adc96b15b94e01107f
Irimitenkan <rohacekd@gmail.com>
2014-03-18 21:49:41 +0100

    Mohu na seriový port posílat informace
    informace se posílají ve formě pole charů

--- PC_site/Arduino/cache_main.cpp
+++ PC_site/Arduino/cache_main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	cout<<"Welcome to the serial test app!\n\n"<<endl;
+	printf("Welcome to the serial test app!\n\n");
 
 	Serial* SP = new Serial("\\\\\.\\COM3");    // adjust as needed
 
 	if (SP->IsConnected())
-		cout<<"We're connected"<<endl;
+		printf("We're connected");
 
 	char incomingData[256] = "";			// don't forget to pre-allocate memory
 	//printf("%s\n",incomingData);
@@ -17,12 +17,15 @@ int main(int argc, char **argv) {
 
 	while(SP->IsConnected())
 	{
-	    char a[1]="";
-		for(int i = 4; i<8;i++) {
-            a[0]=48+i;
-            SP->WriteData(a, 1);
-		}
-		Sleep(1000);
+		readResult = SP->ReadData(incomingData,dataLength);
+		printf("Bytes read: (-1 means no data available) %i\n",readResult);
+
+		std::string test(incomingData);
+
+		printf("%s",incomingData);
+		test = "";
+        SP->WriteData("0", 1);
+		Sleep(500);
 	}
 	return 0;
 }
